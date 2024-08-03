5b127e318786ca615fdf722b3031447f502af832
asuexe <vaghelavivek222222@gmail.com>
2024-08-03 12:23:33 +0530

    first

--- aicontent.py
+++ aicontent.py
@@ -1,27 +1,3 @@
-# import os
-# from urllib import response 
-# import openai
-# import config 
-# openai.api_key = config.OPENAI_API_KEY
-
-# def aicontent(query):
-#     response = openai.chat.completions.create(
-#         model='gpt-2',
-#         prompt="Generate a detailed product description for:{}".format(query),
-#         temperature=0.7,
-#         max_tokens=256,
-#         top_p=1,
-#         frequency_penalty=0,
-#         presence_penalty=0)
-    
-#     if 'choices' in response:
-#         if len(response['choices']) >0:
-#              answer = response['choices'][0]['text']
-#         else:
-#             answer = 'Ugh oh ! i accept i fail !'
-#     return answer
-
-import os
 import openai
 import config
 
@@ -54,21 +30,3 @@ def aicontent(query):
         answer = f"Error: {str(e)}"
     
     return answer
-
-# from transformers import GPT2LMHeadModel, GPT2Tokenizer
-
-# def aicontent(query):
-#     # Load the GPT-2 tokenizer and model from Hugging Face
-#     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-#     model = GPT2LMHeadModel.from_pretrained("gpt2")
-
-#     # Encode the query and generate a response
-#     input_text = "Generate a detailed product description for: " + query
-#     input_ids = tokenizer.encode(input_text, return_tensors='pt')
-
-#     # Generate text
-#     output = model.generate(input_ids, max_length=256, temperature=0.7, top_p=1.0, do_sample=True)
-
-#     # Decode the output and return the result
-#     answer = tokenizer.decode(output[0], skip_special_tokens=True)
-#     return answer
